a374ff70dfe5e081a676c4a4491aa63c0e62a81a
Justin F. Hallett <thesin@southofheaven.org>
2018-02-26 12:55:32 -0700

    Update to multilingual with a few tweaks to start on the theme

--- js/effect.js
+++ js/effect.js
@@ -1,53 +1,53 @@
 // JavaScript Document
 var is_init = false;
 $(window).on('load resize', function() {
-    var odd = true;
-    $.each($('.item'), function() {
-        var dir = 'left';
-        var x = $(this).position();
-        var h = $(this).outerHeight();
+  var odd = true;
+  $.each($('.item'), function() {
+    var dir = 'left';
+    var x = $(this).position();
+    var h = $(this).outerHeight();
 
-		if (is_init === false) {
-        	if (odd === false) {
-        	    dir = 'right';
-        	    odd = true;
-        	} else {
-        	    odd = false;
-        	}
-		}
+    if (is_init === false) {
+      if (odd === false) {
+        dir = 'right';
+        odd = true;
+      } else {
+        odd = false;
+      }
+    }
 
-        $(this).addClass(dir).attr('data-top', x.top).attr('data-bottom', x.top + h);
-    });
-	
-	if (is_init === false)
-		is_init = true;
+    $(this).addClass(dir).attr('data-top', x.top).attr('data-bottom', x.top + h);
+  });
+
+  if (is_init === false)
+    is_init = true;
 });
 
 $(window).on('load resize scroll', function() {
-	var offset = 100;
-    var tpos = $(document).scrollTop();
-    var bpos = tpos + $(window).outerHeight(true);
-    var tot = $(document).outerHeight(true) - $(window).outerHeight(true);
+  var offset = 100;
+  var tpos = $(document).scrollTop();
+  var bpos = tpos + $(window).outerHeight(true);
+  var tot = $(document).outerHeight(true) - $(window).outerHeight(true);
 
-    var minpos = 30;
-    var maxpos = 70;
-    var scrollper = 100 - Math.round((tpos / tot) * 100);
-    var earthpos = ((maxpos / 100) * scrollper) + ((minpos / 100) * (100 - scrollper));
+  var minpos = 30;
+  var maxpos = 70;
+  var scrollper = 100 - Math.round((tpos / tot) * 100);
+  var earthpos = ((maxpos / 100) * scrollper) + ((minpos / 100) * (100 - scrollper));
 
-    $('#earth').css({
-        'top': earthpos + 'vh'
-    });
+  $('#earth').css({
+    'top': earthpos + 'vh'
+  });
 
-    $.each($('.item'), function() {
-        if (bpos - offset > $(this).attr('data-top') && tpos + offset < $(this).attr('data-bottom')) {
-            $(this).addClass('show');
-		} else if (tpos + offset > $(this).attr('data-bottom') || bpos - offset < $(this).attr('data-top')) {
-			$(this).removeClass('show');
-		}
-    });
+  $.each($('.item'), function() {
+    if (bpos - offset > $(this).attr('data-top') && tpos + offset < $(this).attr('data-bottom')) {
+      $(this).addClass('show');
+    } else if (tpos + offset > $(this).attr('data-bottom') || bpos - offset < $(this).attr('data-top')) {
+      $(this).removeClass('show');
+    }
+  });
 
-    if (tpos >= tot)
-        $('#footer').addClass('bottom');
-    else
-        $('#footer').removeClass('bottom');
+  if (tpos >= tot)
+    $('#footer').addClass('bottom');
+  else
+    $('#footer').removeClass('bottom');
 });
